1a1db49b5b5da06aefbc0d79b62a8ec6d36853b3
margauxperez <margarita.perez@obf.ateneo.edu>
2020-03-09 20:29:44 +0800

    Fixed the heroes/tests file

--- heroes/tests.py
+++ heroes/tests.py
@@ -1,3 +1,24 @@
+#heroes/tests.py
+
 from django.test import TestCase
 
-# Create your tests here. testing change
+# Create your tests here. 
+class HomepageTest(TestCase):
+	def homepage_test(self):
+		response = self.client.get('/heroes/')
+		self.assertTemplateUsed(response, 'homepage.html')
+
+class CloudTest(TestCase):
+	def cloud_test(self):
+		response = self.client.get('/hero/cloud/')
+		self.assertTemplateUsed(response, 'detail_cloud.html')
+
+class SunfloweyTest(TestCase):
+	def sunflowey_test(self):
+		response = self.client.get('/hero/sunflowey/')
+		self.assertTemplateUsed(response, 'detail_sunflowey.html')
+
+class JesterTest(TestCase):
+	def jester_test(self):
+		response = self.client.get('/hero/jester/')
+		self.assertTemplateUsed(response, 'detail_jester.html')
