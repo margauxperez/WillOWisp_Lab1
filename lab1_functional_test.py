90dd0ba797d850ba467764a24fa57b1115145d41
margauxperez <margarita.perez@obf.ateneo.edu>
2020-03-09 23:23:01 +0800

    Making ~1/3 of the tests in LFT

--- lab1_functional_test.py
+++ lab1_functional_test.py
@@ -23,9 +23,39 @@ class NewVisitorTest(unittest.TestCase):
 		
 		# She sees a list containing three heroes with their corresponding 
 		# names, health points, and damage
+		#names
+		cloud_text = self.browser.find_element_by_id('cloud')
+		self.assertIn('Cloud', cloud_text.text)
+		jester_text = self.browser.find_element_by_id('jester')
+		self.assertIn('Jester', jester_text.text)
+		sunflowey_text = self.browser.find_element_by_id('sunflowey')
+		self.assertIn('Sunflowey', sunflowey_text.text)
+		#health points
+		cloud_health = self.browser.find_element_by_id('cloudHealth')
+		self.assertIn('Cloud', cloud_health.text)
+		jester_health = self.browser.find_element_by_id('jesterHealth')
+		self.assertIn('Jester', jester_health.text)
+		sunflowey_health = self.browser.find_element_by_id('sunfloweyHealth')
+		self.assertIn('Sunflowey', sunflowey_health.text)
+		#damage
+		cloud_damage = self.browser.find_element_by_id('cloudDamage')
+		self.assertIn('Cloud', cloud_damage.text)
+		jester_damage = self.browser.find_element_by_id('jesterDamage')
+		self.assertIn('Jester', jester_damage.text)
+		sunflowey_damage = self.browser.find_element_by_id('sunfloweyDamage')
+		self.assertIn('Sunflowey', sunflowey_damage.text)
+
 
 		# When she selects one of the heroes, she is sent to another page
 		# containing more information about the hero (additional stats, lore, image).
+		#cloud_stat = self.browser.find_element_by_id('cloud')
+		#self.assertIn('Cloud', cloud_text.text)
+
+		#jester_text = self.browser.find_element_by_id('jester')
+		#self.assertIn('Jester', jester_text.text)
+
+		#sunflowey_text = self.browser.find_element_by_id('sunflowey')
+		#self.assertIn('Sunflowey', sunflowey_text.text)
 		
 		# She spots the page title and header mentions the name of the hero she selected.
 		
