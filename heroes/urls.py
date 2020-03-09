5c1eb120dbc03eae4366bec2318c5ddff66b0496
margauxperez <margarita.perez@obf.ateneo.edu>
2020-03-09 21:27:04 +0800

    FINAL LEGIT fixed heroes/urls

--- heroes/urls.py
+++ heroes/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls import url
 from .views import HomepageView, CloudView, SunfloweyView, JesterView
 
 urlpatterns = [
-	#url(r'^$', HomepageView.as_view(), name='homepage'),
+	url(r'^$', HomepageView.as_view(), name='homepage'),
 	url(r'heroes/', HomepageView.as_view(), name='homepage'),
-	url(r'/hero/cloud/', CloudView.as_view(), name='detail_cloud'),
-	url(r'/hero/sunflowey/', SunfloweyView.as_view(), name='detail_sunflower'),
-	url(r'/hero/jester/', JesterView.as_view(), name='detail_jester'),
+	url(r'hero/cloud', CloudView.as_view(), name='detail_cloud'),
+	url(r'hero/sunflowey', SunfloweyView.as_view(), name='detail_sunflower'),
+	url(r'hero/jester', JesterView.as_view(), name='detail_jester'),
 ]
